6fe665b28f3824d783753e76061bdd80c916082c
mladen91 <mladen.teofilovic@bitcamp.ba>
2015-06-21 11:01:02 +0200

    Solve task2 for Homework Weekend 5

---
+++ bitcampHomeworkWeekend05/src/ba/bitcamp/homeworkweekend05/task02/Event.java
@@ -0,0 +1,12 @@
+package ba.bitcamp.homeworkweekend05.task02;
+
+public class Event {
+
+	// Constant values for events
+	public static final int DEFEAT = 1;
+	public static final int FRIENDSHIP = 2;
+	public static final int LOVE = 3;
+	public static final int VENGEANCE = 4;
+	public static final int MANIPULATION = 5;
+
+}
